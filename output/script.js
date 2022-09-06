9d19f6f303a4d8598356cbe17055f9bee41b4ceb
Biddut <biddut@noobyia.email>
2022-09-06 11:44:55 +0600

    Type Basic

--- output/script.js
+++ output/script.js
@@ -1,4 +1,47 @@
 "use strict";
-console.log("hello world!");
-const country = "Bangladesh";
-console.log(country);
+// let playerName = "Masrafi";
+// let age = 35;
+// console.log(playerName);
+// playerName = 34;
+// age = 34;
+// age = "Sakib";
+// console.log(playerName);
+// let playerName;
+// console.log(playerName);
+// playerName = "Sakib";
+// console.log(playerName);
+// playerName = 34;
+// console.log(playerName);
+function multiply(a, b) {
+    return a * b;
+}
+console.log(multiply(3, 4));
+// array
+// let fruits = ['apple', 'banan', 'mango'];
+// fruits.push(34);
+// fruits.push('orange');
+let fruits = [];
+fruits.push(34);
+fruits.push('orange');
+// let mixed = ['apple', 34, true];
+// mixed.push(34);
+// mixed.push('orange');
+// mixed.push({
+//     name: "sakib",
+// });
+let mixed = ['apple', 34, true, {}];
+mixed.push(34);
+mixed.push('orange');
+mixed.push({
+    name: "sakib",
+});
+// object
+let person = {
+    name: "Masrafi",
+    age: 34,
+    isCaptain: true,
+};
+person.name = "Sakib";
+//  person.name = 34;
+person.age = 34;
+person.country = "Bangladesh";
--- src/script.ts
+++ src/script.ts
@@ -1,3 +1,69 @@
-console.log("hello world!");
-const country = "Bangladesh";
-console.log(country);
+// let playerName = "Masrafi";
+// let age = 35;
+
+// console.log(playerName);
+
+// playerName = 34;
+// age = 34;
+// age = "Sakib";
+// console.log(playerName);
+
+
+// let playerName;
+
+// console.log(playerName);
+
+// playerName = "Sakib";
+
+// console.log(playerName);
+
+// playerName = 34;
+
+
+// console.log(playerName);
+
+function multiply(a: number, b: number){
+    return a*b;
+}
+
+console.log(multiply(3, 4));
+
+// array
+
+// let fruits = ['apple', 'banan', 'mango'];
+
+// fruits.push(34);
+// fruits.push('orange');
+
+let fruits = [];
+
+fruits.push(34);
+fruits.push('orange');
+
+// let mixed = ['apple', 34, true];
+
+// mixed.push(34);
+// mixed.push('orange');
+// mixed.push({
+//     name: "sakib",
+// });
+
+let mixed = ['apple', 34, true, {}];
+
+mixed.push(34);
+mixed.push('orange');
+mixed.push({
+    name: "sakib",
+});
+
+// object
+ let person = {
+    name: "Masrafi",
+    age: 34,
+    isCaptain: true,
+ }
+
+ person.name = "Sakib";
+//  person.name = 34;
+ person.age = 34;
+ person.country = "Bangladesh";
