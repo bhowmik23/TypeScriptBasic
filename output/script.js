e09257d664a85eb958df18f7c626c99e2c61add1
Biddut <biddut@noobyia.email>
2022-09-06 12:11:44 +0600

    Explicit And UnionTypes

--- output/script.js
+++ output/script.js
@@ -1,47 +1,43 @@
 "use strict";
-// let playerName = "Masrafi";
-// let age = 35;
-// console.log(playerName);
-// playerName = 34;
-// age = 34;
-// age = "Sakib";
-// console.log(playerName);
-// let playerName;
-// console.log(playerName);
-// playerName = "Sakib";
-// console.log(playerName);
-// playerName = 34;
-// console.log(playerName);
-function multiply(a, b) {
-    return a * b;
-}
-console.log(multiply(3, 4));
-// array
-// let fruits = ['apple', 'banan', 'mango'];
-// fruits.push(34);
-// fruits.push('orange');
-let fruits = [];
-fruits.push(34);
-fruits.push('orange');
-// let mixed = ['apple', 34, true];
-// mixed.push(34);
-// mixed.push('orange');
-// mixed.push({
-//     name: "sakib",
-// });
-let mixed = ['apple', 34, true, {}];
-mixed.push(34);
-mixed.push('orange');
-mixed.push({
-    name: "sakib",
-});
+// let a;
+// a = 34;
+// a = "hello";
+// let a: string;
+// let b: number;
+// a = 4;
+// a = "hello";
+// b = 4;
+// b = "hello";
+// Array
+// let a :string[] = [];
+// a.push(4);
+// a.push('hello');
+// let a :number[] = [];
+// a.push(4);
+// a.push('hello');
+// normal variable
+let b;
+b = 3;
+b = 'hello';
+let a = [];
+a.push(4);
+a.push('hello', 34);
+a.push('hello', 34, true);
 // object
-let person = {
-    name: "Masrafi",
-    age: 34,
-    isCaptain: true,
+// let c: object;
+// c = {
+//     name: 'hello',
+// }
+let c;
+c = {
+    name: 'hello',
+    age: 'hello',
+    addult: 'hello',
 };
-person.name = "Sakib";
-//  person.name = 34;
-person.age = 34;
-person.country = "Bangladesh";
+c = {
+    name: 'hello',
+    age: 32,
+    addult: true,
+};
+let d;
+d = [1, 2, 3, 4];
